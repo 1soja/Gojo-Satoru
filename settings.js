304bdf38785831d9c5a563992e9acd7fcbcd362e
1soja <90239033+1soja@users.noreply.github.com>
2022-12-17 21:50:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['917306004019']
+global.premium = ['917306004019']
+global.ownernomer = '917306004019'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
